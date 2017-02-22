dcec182b33d2657f920c19589263e9d6389e38ce
jaredebird <bird.jared@gmail.com>
2017-02-21 21:41:26 -0500

    Add files via upload

--- Assignment3/Breakout.java
+++ Assignment3/Breakout.java
@@ -15,19 +15,21 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import com.sun.glass.ui.Screen;
+
 public class Breakout extends GraphicsProgram {
 
 /** Width and height of application window in pixels */
 	public static final int APPLICATION_WIDTH = 800;
-	public static final int APPLICATION_HEIGHT = 1200;
+	public static final int APPLICATION_HEIGHT = 1000;
 
 /** Dimensions of game board (usually the same) */
 	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 /** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
+	private static final int PADDLE_WIDTH = 200;
+	private static final int PADDLE_HEIGHT = 30;
 
 /** Offset of the paddle up from the bottom */
 	private static final int PADDLE_Y_OFFSET = 30;
@@ -36,7 +38,9 @@ public class Breakout extends GraphicsProgram {
 	private static final int NBRICKS_PER_ROW = 10;
 
 /** Number of rows of bricks */
-	private static final int NBRICK_ROWS = 10;
+	private static final int NBRICK_ROWS = 1;
+	
+	private static final int NBRICKS = NBRICKS_PER_ROW * NBRICK_ROWS;
 
 /** Separation between bricks */
 	private static final int BRICK_SEP = 8;
@@ -61,7 +65,10 @@ public class Breakout extends GraphicsProgram {
 	private static final int BALL_START_Y = 500;
 	
 	/** Vertical Velocity*/
-	private static final int Y_VELOCITY = 3;
+	private static final int Y_VELOCITY = 4;
+
+	/** Pause Time */
+	private static final int PAUSE = 6;
 	
 	
 /* Method: run() */
@@ -70,11 +77,35 @@ public class Breakout extends GraphicsProgram {
 		/* You fill this in, along with any subsidiary methods */
 		setupBoard();
 		runGame();
+
 	}
 
 private void runGame() {
 	// TODO Auto-generated method stub
-	
+int i = 0;
+	while (!gameOver()){
+		i++;
+		label.setLabel(Integer.toString(i) + " - " + ball.getBricks());
+		while (ball.isVisible() && !gameOver()){
+			ball.moveBall();
+			checkCollision();
+			pause(4);
+			label.setLabel(Integer.toString(i) + " - " + ball.getBricks());
+//			label.setLabel(String.valueOf(ball.getBottom()) + " - " + HEIGHT);
+			if (ball.getBottom() > HEIGHT) {
+				ball.setVisible(false);
+			}
+		}
+	}
+}
+
+private boolean gameOver() {
+	// TODO Auto-generated method stub
+	if (ball.getRound() < NTURNS && ball.getBricks() < NBRICKS) {
+		return false;
+	}else{
+		return true;
+	}
 }
 
 private void setWindowSize() {
@@ -84,55 +115,151 @@ private void setWindowSize() {
 	private void setupBoard() {
 	
 		setWindowSize();
-		pause(30);
+		createBall();
 		createBricks();
 		createPaddle();
-		createBall();
+		label = new GLabel("Start");
+		label.setFont("Times New Roman-36");
+		add(label, 50,50);
 		addMouseListeners();
 	}
 	private void createBall(){
-		ball = new GOval(BALL_RADIUS*2,BALL_RADIUS*2);
+		ball = new pinBall(BALL_RADIUS*2,BALL_RADIUS*2);
 		ball.setVisible(false);
 //		ball.setBounds(getWidth()/2-BALL_RADIUS, BALL_START_Y);
 		ball.setBounds(getWidth()/2-BALL_RADIUS, BALL_START_Y, BALL_RADIUS*2,BALL_RADIUS*2);
 		ball.setFilled(true);
 		ball.setColor(Color.BLACK);
 		add(ball);
+		ball.setRound(currentRound);
 	}
 	private void createPaddle() {
-		 paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
 		paddle.setFilled(true);
 		paddle.setColor(Color.BLACK);
-		add(paddle, getWidth()/2, getHeight()-PADDLE_Y_OFFSET);
+		add(paddle, getWidth()/2, HEIGHT-PADDLE_Y_OFFSET);
 		
 		
 	}
 
 	public void mouseMoved(MouseEvent e) {
-			paddle.setLocation(e.getX()-paddle.getWidth()/2, paddle.getY());
+			paddle.setLocation(median(e.getX()-paddle.getWidth()/2, WIDTH - PADDLE_WIDTH, 0 ), paddle.getY());
 	}
 	public void mouseClicked(MouseEvent e){
 		
 		activateBall();
-		startBall();
-		while (ball.isVisible()){
-		moveBall();
-		checkCollision();
-		}
+
 		
 	}
+	private double median (double a, double b, double c){
+		return Math.max(Math.min(a,b), Math.min(Math.max(a,b),c));
+	}
 
+	private void checkCollision() {
+		checkPaddle();
+		checkRightWall();
+		checkLeftWall();
+		checkTopWall();
+		checkBrick();
+	}
+
+	private void checkBrick() {
+		checkBrickTop();
+		checkBrickBottom();
+		checkBrickRight();
+		checkBrickLeft();
+	}
+
+	private void checkBrickLeft() {
+		GObject collobj = getElementAt(ball.getLeft(), ball.getCenterY());
+		if (collobj != null && collobj.getHeight() == BRICK_HEIGHT && collobj.getWidth() == BRICK_WIDTH){
+			if (ball.getVx() < 0) {
+				ball.reverseY();
+			}
+			remove(collobj);
+			collobj = null;
+			ball.hitBrick();
+		}
+	}
+
+	private void checkBrickRight() {
+		GObject collobj = getElementAt(ball.getRight(), ball.getCenterY());
+		if (collobj != null && collobj.getHeight() == BRICK_HEIGHT && collobj.getWidth() == BRICK_WIDTH){
+			if (ball.getVx() > 0) {
+				ball.reverseY();
+			}
+			remove(collobj);
+			collobj = null;
+			ball.hitBrick();
+		}
+	}
+
+	private void checkBrickBottom() {
+		GObject collobj = getElementAt(ball.getCenterX(), ball.getBottom());
+		if (collobj != null && collobj.getHeight() == BRICK_HEIGHT && collobj.getWidth() == BRICK_WIDTH){
+			if (ball.getVy() > 0) {
+				ball.reverseY();
+			}
+			remove(collobj);
+			collobj = null;
+			ball.hitBrick();
+		}
+	}
+
+	private void checkBrickTop() {
+		GObject collobj = getElementAt(ball.getCenterX(), ball.getTop());
+		if (collobj != null && collobj.getHeight() == BRICK_HEIGHT && collobj.getWidth() == BRICK_WIDTH){
+			if (ball.getVy() < 0) {
+				ball.reverseY();
+			}
+			remove(collobj);
+			collobj = null;
+			ball.hitBrick();
+		}
+	}
+
+	private void checkPaddle() {
+		GObject collobj = getElementAt(ball.getCenterX(), ball.getBottom());
+		if (collobj == paddle){
+			if (ball.getVy() > 0) {
+				ball.hitPaddle(collobj.getX(), collobj.getX()+collobj.getWidth());
+			}
+		}
+	}
+	private void checkRightWall(){
+		if (ball.getRight() > WIDTH){
+			if (ball.getVx() > 0){ ball.reverseX();}
+		}		
+	}
+	private void checkLeftWall(){
+		if (ball.getLeft() <= 0){
+			if (ball.getVx() < 0 ){ball.reverseX();}
+		}		
+	}
+	private void checkTopWall(){
+		if (ball.getTop() <= 0){
+			if (ball.getVy() < 0 ){ ball.reverseY();}	
+		}
+	}	
+		
 
 	private void startBall() {
 		
-		vx = rgen.nextDouble(1.0, 3.0);
-		if (rgen.nextBoolean(0.5)) vx = -vx;
-		vy = Y_VELOCITY;
+
 	}
 
 	private void activateBall() {
-		if (!ball.isVisible()){
+		if (!ball.isVisible() && currentRound < NTURNS){
 			ball.setVisible(true);
+			ball.setLocation(getWidth()/2-BALL_RADIUS, BALL_START_Y);
+			currentRound ++;
+			ball.setRound(currentRound);
+			vx = rgen.nextDouble(1.0, 3.0);
+			if (rgen.nextBoolean(0.5)) vx = -vx;
+				ball.setVelocity(vx, Y_VELOCITY);
+				
+		} else if (currentRound == NTURNS) {
+			label.setLabel("Game Over!");
 		}
 		
 	}
@@ -198,8 +325,9 @@ private void setWindowSize() {
 	return rowColors;
 }
 	private GRect paddle;
-	private double vx, vy;
-	private GOval ball;
+	private pinBall ball;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-
+	private GLabel label;
+	private int currentRound;
+	private double vx, vy;
 }
---
+++ Assignment3/pinBall.java
@@ -0,0 +1,94 @@
+import acm.graphics.*;
+/** Separation between bricks */
+
+
+public class pinBall extends GOval {
+	
+	private static final int maxV = 8;
+	private static final int maxChangeV = 3;
+	
+	public pinBall(double width, double height) {
+		super(width, height);
+		vX = 0;
+		vY=0;
+		bricks = 0;
+		// TODO Auto-generated constructor stub
+	}
+	public double getLeft() {
+		return this.getX();
+	}
+	public double getRight() {
+		return this.getX()+this.getWidth();
+	}
+	public double getTop() {
+		return this.getY();
+	}
+	public double getBottom() {
+		return this.getY() + this.getHeight();
+	}
+	public double getCenterX() {
+		return this.getX() + this.getWidth()/2;
+	}
+	public double getCenterY() {
+		return this.getY() + this.getHeight()/2;
+	}
+	public boolean rightBoundry(double rightBorder) {
+		if (this.getRight() >= rightBorder) { 
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public void hitPaddle(double paddleLeft, double paddleRight){
+		double paddleCenter = (paddleLeft + paddleRight)/2;
+		double paddleWidth = paddleRight - paddleLeft;
+		double vChange = maxChangeV * ((this.getCenterX() - paddleCenter)/(paddleWidth/2.0));
+		vX = median (-1 * maxV, maxV, vX + vChange);
+		vY = -vY;
+	}
+	private double median (double a, double b, double c){
+		return Math.max(Math.min(a,b), Math.min(Math.max(a,b),c));
+	}
+	
+	public void moveBall(){
+		this.move(vX, vY);
+	}
+	public void setVx(double xVel){
+		vX = (int) xVel;
+	}
+	public double getVx(){
+		return vX;
+	}
+	public double getVy(){
+		return vY;
+	}
+	public void setVy(double yVel){
+		vY = yVel;
+	}
+	public void setVelocity(double xVel, double yVel) {
+		vX = xVel;
+		vY = yVel;
+	}
+	public int getBricks(){
+		return bricks;
+	}
+	public void hitBrick(){
+		bricks = bricks + 1;
+	}
+	public void setRound(int round){
+		currentRound = round;
+	}
+	public int getRound(){
+		return currentRound;
+	}
+	public void reverseX() {
+		vX = -vX;
+	}
+	public void reverseY(){
+		vY = -vY;
+	}
+	private  double vX, vY;
+	private int currentRound, bricks;
+	
+}
